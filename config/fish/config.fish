0c2ea876aab3ebdbf45f3185f316b516d2ca3b24
Tomáš Milata <tomas.milata@gmail.com>
2017-11-16 22:50:34 +0000

    Add config that allows using things like 'conda activate my-env'

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 set -x PATH $PATH ~/bin
 
 set -x PATH "$HOME/miniconda3/bin" $PATH
-
+set -U fish_user_paths $HOME/miniconda3/bin $fish_user_paths
+source (conda info --root)/etc/fish/conf.d/conda.fish
 
 
