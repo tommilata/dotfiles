6bce4b53c0c007cd75e8fa2983fd75cea2df483a
Tomáš Milata <tomas.milata@gmail.com>
2017-12-08 13:38:21 +0000

    Improve k8scontext

--- config/fish/functions/k8scontext.fish
+++ config/fish/functions/k8scontext.fish
@@ -1,3 +1,3 @@
 function k8scontext
-	kubectl config get-contexts | grep asidata | cut -c 2- | awk '{print $1}' | fzy | xargs kubectl config use-context
+	kubectl config get-contexts | tail -n +2 | cut -c 2- | awk '{print $1}' | fzy | xargs kubectl config use-context
 end
