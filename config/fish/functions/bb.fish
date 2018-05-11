4d5eff52578f92976565d472cfb03e4e4b03670c
Tomáš Milata <tomas.milata@gmail.com>
2018-05-11 13:18:56 +0100

    Add bitbucket helper

--- config/fish/functions/bb.fish
+++ config/fish/functions/bb.fish
@@ -1,6 +1,7 @@
 function bb
 	find ~/src/bitbucket.org -type d -mindepth 2 -maxdepth 2 -not -path '*/\.*' \
 		| awk -F'/' '{ print $(NF-1) "/" $NF }' \
-		| fzf \
-		| awk '{ print "https://bitbucket.org/" $0 }'
+		| fzy \
+		| awk '{ print "https://bitbucket.org/" $0 }' \
+		| xargs open
 end
