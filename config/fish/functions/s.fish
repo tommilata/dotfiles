a568b6d918297afe4944dfadcefb6c242b04d903
Tomáš Milata <tomas.milata@gmail.com>
2018-05-16 22:50:03 +0100

    Strip common prefix from `s` output

--- config/fish/functions/s.fish
+++ config/fish/functions/s.fish
@@ -1,3 +1,4 @@
 function s
-	find ~/src -type d -maxdepth 4 -not -path '*/\.*' | fzy | read dir; and cd $dir
+	cd ~/src
+	find * -type d -maxdepth 3 -not -path '*/\.*' | fzy | read dir; and cd $dir
 end
