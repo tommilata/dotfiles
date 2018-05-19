7efa8ec930708eebb247538743473ec180a75787
Tomáš Milata <tomas.milata@gmail.com>
2018-05-19 01:42:31 +0100

    Read bb repo from argument

--- config/fish/functions/bb.fish
+++ config/fish/functions/bb.fish
@@ -1,7 +1,31 @@
 function bb
-	find ~/src/bitbucket.org -type d -mindepth 2 -maxdepth 2 -not -path '*/\.*' \
-		| awk -F'/' '{ print $(NF-1) "/" $NF }' \
+	for arg in $argv
+		switch "$arg"
+			case -p --pull-requests
+				set page '/pull-requests'
+			case \*
+				set dir (realpath "$arg")
+				set repo (echo $dir | repo_from_dir )
+		end
+	end
+
+	if not set -q dir;
+		find ~/src/bitbucket.org -type d \
+			-mindepth 2 -maxdepth 2 \
+			-not -path '*/\.*' \
+		| repo_from_dir \
 		| fzy \
-		| awk '{ print "https://bitbucket.org/" $0 }' \
-		| xargs open
+		| read repo
+
+	end
+
+	echo $repo \
+	| awk -v page="$page" '{ print "https://bitbucket.org/" $0 page }' \
+	| xargs open
+end
+
+function repo_from_dir
+	while read line
+		echo $line | awk -F'/' '{ print $(NF-1) "/" $NF }' <&1 
+	end
 end
