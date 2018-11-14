0a5d9379977fd5d4d14645f152da0ccf82e44319
Tomáš Milata <tomas.milata@gmail.com>
2018-11-14 23:51:17 +0000

    Make `git lg` look nicer when the output is wide

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 # cargo
-[ -d "$HOME/.cargo/bin" ]; and set -x PATH "$HOME/.cargo/bin" $PATH
+#[ -d "$HOME/.cargo/bin" ]; and set -x PATH "$HOME/.cargo/bin" $PATH
 
 # golang
 set -x -U GOPATH $HOME
@@ -15,7 +15,7 @@ set -U fish_user_paths $HOME/miniconda3/bin $fish_user_paths
 
 # more robust would be to prefix with  /(conda info --root)
 # but it's quite slow
-source /Users/tomas/miniconda3/etc/fish/conf.d/conda.fish
+# source /Users/tomas/miniconda3/etc/fish/conf.d/conda.fish
 
 # bobthefish
 set -g theme_display_k8s_context no
@@ -28,15 +28,16 @@ alias stree='/Applications/SourceTree.app/Contents/Resources/stree'
 set -g fish_user_paths "/usr/local/opt/postgresql@9.5/bin" $fish_user_paths
 
 # appengine
-if [ -d ~/appengine-java-sdk/bin ]
-	set -x PATH $PATH ~/appengine-java-sdk/bin
-end
+#if [ -d ~/appengine-java-sdk/bin ]
+#	set -x PATH $PATH ~/appengine-java-sdk/bin
+#end
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/tomas/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/tomas/google-cloud-sdk/path.fish.inc'; else; . '/Users/tomas/google-cloud-sdk/path.fish.inc'; end; end
+#if [ -f '/Users/tomas/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/tomas/google-cloud-sdk/path.fish.inc'; else; . '/Users/tomas/google-cloud-sdk/path.fish.inc'; end; end
 
 
 
 alias preview="fzf --preview 'bat {}'"
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
+
