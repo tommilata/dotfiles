b25a8d65d937f4690d03f60fa8094696db8ffae1
Tomáš Milata <tomas.milata@gmail.com>
2019-03-19 12:01:29 +0100

    Improve fish startup time

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,8 @@
 #[ -d "$HOME/.cargo/bin" ]; and set -x PATH "$HOME/.cargo/bin" $PATH
 
 # golang
-set -x -U GOPATH $HOME
-set -x -U GOBIN $GOPATH/bin
+set -x GOPATH $HOME
+set -x GOBIN $GOPATH/bin
 
 # do not display the welcome message
 set -e fish_greeting
@@ -11,11 +11,10 @@ set -e fish_greeting
 set -x PATH $PATH ~/bin
 
 set -x PATH "$HOME/miniconda3/bin" $PATH
-set -U fish_user_paths $HOME/miniconda3/bin $fish_user_paths
 
 # more robust would be to prefix with  /(conda info --root)
 # but it's quite slow
-# source /Users/tomas/miniconda3/etc/fish/conf.d/conda.fish
+source /Users/tomas/miniconda3/etc/fish/conf.d/conda.fish
 
 # bobthefish
 set -g theme_display_k8s_context no
@@ -25,7 +24,7 @@ set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 alias stree='/Applications/SourceTree.app/Contents/Resources/stree'
 
 
-set -g fish_user_paths "/usr/local/opt/postgresql@9.5/bin" $fish_user_paths
+set -x PATH "/usr/local/opt/postgresql@9.5/bin" $PATH
 
 # appengine
 #if [ -d ~/appengine-java-sdk/bin ]
