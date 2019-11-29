1d8c9a6c7fe59a1b90395d18e2eb05f3ded0cec0
Tomáš Milata <tomas.milata@gmail.com>
2019-11-29 14:42:14 +0100

    Cleanup fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,41 +1,48 @@
 # cargo
 #[ -d "$HOME/.cargo/bin" ]; and set -x PATH "$HOME/.cargo/bin" $PATH
 
+# brew
+set -x HOMEBREW_NO_ANALYTICS 1
+
 # golang
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
-
 # do not display the welcome message
 set -e fish_greeting
-
-set -x PATH $PATH ~/bin
-
-set -x PATH "$HOME/miniconda3/bin" $PATH
-
-# more robust would be to prefix with  /(conda info --root)
-# but it's quite slow
-source /Users/tomas/miniconda3/etc/fish/conf.d/conda.fish
-
 # bobthefish
 set -g theme_display_k8s_context no
 set -g theme_powerline_fonts no
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
+# PATH
+set -x PATH $PATH ~/bin
+set -x PATH "$HOME/miniconda3/bin" $PATH
+set -x PATH "/usr/local/opt/postgresql@9.5/bin" $PATH
+
+# pipx
+register-python-argcomplete --shell fish pipx | .
+# pyenv-virtualenv
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
+# added by pipsi (https://github.com/mitsuhiko/pipsi)
+set -x PATH /Users/tomas/.local/bin $PATH
+# conda
+# more robust would be to prefix with  /(conda info --root)
+# but it's quite slow
+source /Users/tomas/miniconda3/etc/fish/conf.d/conda.fish
+
 # SourceTree
 alias stree='/Applications/SourceTree.app/Contents/Resources/stree'
-
-
-set -x PATH "/usr/local/opt/postgresql@9.5/bin" $PATH
 
 # appengine
 #if [ -d ~/appengine-java-sdk/bin ]
 #	set -x PATH $PATH ~/appengine-java-sdk/bin
 #end
-
 # The next line updates PATH for the Google Cloud SDK.
-#if [ -f '/Users/tomas/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/tomas/google-cloud-sdk/path.fish.inc'; else; . '/Users/tomas/google-cloud-sdk/path.fish.inc'; end; end
+if [ -f '/Users/tomas/google-cloud-sdk/path.fish.inc' ]; . '/Users/tomas/google-cloud-sdk/path.fish.inc'; end
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
-
+# git
 alias copy-git-describe='echo -n (git describe --dirty=-SNAPSHOT) | pbcopy'
 alias copy-git-branch='echo -n (git symbolic-ref --short HEAD) | pbcopy'
 
@@ -43,16 +50,11 @@ alias preview="fzf --preview 'bat {}'"
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
 
-
-# added by pipsi (https://github.com/mitsuhiko/pipsi)
-set -x PATH /Users/tomas/.local/bin $PATH
-
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/tomas/google-cloud-sdk/path.fish.inc' ]; . '/Users/tomas/google-cloud-sdk/path.fish.inc'; end
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
-# pipx
-register-python-argcomplete --shell fish pipx | .
-
+# exa
+if type exa >/dev/null;
+  alias l='exa -l --group-directories-first --git'
+  alias ll='exa -l --all --all --group-directories-first --git'
+  alias lt='exa -T --git-ignore --level=2 --group-directories-first'
+  alias llt='exa -lT --git-ignore --level=2 --group-directories-first'
+  alias lT='exa -T --git-ignore --level=4 --group-directories-first'
+end
