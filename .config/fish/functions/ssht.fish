d14e32c794961c4488d7be1f2708d42b274393cd
Tomáš <tomas.milata@gmail.com>
2020-01-24 12:21:46 +0100

    Add ssh + tmux -CC + fzy helper

---
+++ .config/fish/functions/ssht.fish
@@ -0,0 +1,11 @@
+function _ssht
+end
+
+
+function ssht
+    set -l host (cat ~/.ssh/config | grep Host | grep -v -i  HostName | grep -v '*' | awk '{ print $2 }' | fzy)
+    ssh -t $host "tmux -CC new-session -A -s default-session"
+  
+end
+
+
